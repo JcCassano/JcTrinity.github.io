77ee5ed4d84a42936516b9db4aa6a57374f16d24
Joseph <78464773+JcTrinity@users.noreply.github.com>
2021-12-22 17:13:15 +0800

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,19 @@
+function toggleMobileMenu(menu) {
+
+    menu.classList.toggle('open')
+}
+
+// var x = document.getElementById("mobile-menu");
+      // if (x.style.display === "block") {
+      //   x.style.display = "none";
+      //   document.getElementById("index_text").style.marginTop ="0px";
+      //   document.getElementById("index_btn").style.marginTop ="0px";
+
+      // }
+      // else {
+      //   x.style.display = "block";
+      //   document.getElementById("index_text").style.marginTop ="80px";
+      //   document.getElementById("index_btn").style.marginTop ="80px";
+      // }
+
+// menu.classList.toggle('open');
